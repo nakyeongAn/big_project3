b201b85dca9ccb5e170dd513200cd672c1f3906c
Anne <dksakdrh@naver.com>
2023-12-29 16:55:58 +0900

    project_urls

--- project/project/urls.py
+++ project/project/urls.py
@@ -23,8 +23,12 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('chat.urls')),
     path('accounts/', include('accounts.urls')),
+    path('accounts/', include('allauth.urls')),
     path('forum/', include('forum.urls')),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL , document_root = settings.STATIC_ROOT)
+    
+    
+    
