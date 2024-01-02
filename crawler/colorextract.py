338182f1e45eb50aaa3ede862ede10fae0016e56
ywseok <kaelriu@gmail.com>
2024-01-02 11:35:35 +0900

    Add comment in crawler

--- crawler/colorextract.py
+++ crawler/colorextract.py
@@ -2,18 +2,21 @@ import numpy as np
 import pandas as pd
 from colorthief import ColorThief
 import os
-
+# 이미지를 기준으로 읽어올 파일 이름 지정
 imgpath='./imgs'
 folderlist=os.listdir(imgpath)
 
 for folder in folderlist:
+    # 각 폴더 내 이미지 파일 불러오기
     result=[]
     imglist=os.listdir(imgpath+'/'+folder)
     for img in imglist:
+        # 개별 이미지 대표 색 5개 추출 후 대표색만 RGB HEX코드로 저장
         now_img_path=os.path.join(imgpath+'/'+folder, img)
         color_check=ColorThief(now_img_path)
         palette=color_check.get_palette(color_count=5)
         result.append([format(palette[0][0],'X')+format(palette[0][1],'X')+format(palette[0][2],'X')])
+    #기존 CSV 파일에 컬러 추출 결과 저장
     color_array=np.array(result)
     color_pd=pd.DataFrame(color_array, columns=['RGB_Code'])
     csv_path=os.path.join('./product_infos/',folder+'_product_infos.csv')
