030ab470d9b3445d645cd574811447b1e4cbfa11
Anne <dksakdrh@naver.com>
2024-01-02 17:29:14 +0900

    로그인, 회원가입 html 수정(로그인 css 입히기

--- project/project/urls.py
+++ project/project/urls.py
@@ -21,7 +21,7 @@ from django.conf import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    #path('', include('chat.urls')),
+    path('', include('chat.urls')),
     path('accounts/', include('accounts.urls')),
     path('accounts/', include('allauth.urls')),
     path('forum/', include('forum.urls')),
