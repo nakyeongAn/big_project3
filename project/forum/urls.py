25aec0d81e6abffe07dff44f7321fca4a9f1a56b
Anne <dksakdrh@naver.com>
2024-01-05 18:25:58 +0900

    공지사항, 질문게시판 html,css

--- project/forum/urls.py
+++ project/forum/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 from .  import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 app_name = 'forum'
 
 urlpatterns = [
-    path('', views.board , name='board'),
+    path('board/', views.board , name='board'),
+    path('notice/', views.notice , name='notice'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL , document_root = settings.STATIC_ROOT)
--- project/forum/views.py
+++ project/forum/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def board(request):
     return render(request, 'forum/board.html')
+
+def notice(request):
+    return render(request, 'forum/notice.html')
